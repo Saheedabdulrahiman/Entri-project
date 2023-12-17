5dde41ddf7d81c3506a4984f34b653d5e8293429
Saheedabdulrahiman <saheeda342@gmail.com>
2023-12-17 22:29:28 +0530

    Revert "Second Commit"
    This reverts commit fec6ec27258648dd5032e68f94e87b5cfa9acbdb.

--- Recipe-app/server/src/models/index.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.promise = global.promise;
-const db = {};
-db.mongoose = mongoose;
-db.user =require('./user.model')(mongoose);
-
-
-
-module.exports= db;
